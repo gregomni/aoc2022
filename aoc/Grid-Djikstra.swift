3f2e5da78c3b004692aaa522149fb884dd6a0644
Greg Titus <toon@omnigroup.com>
2024-12-22 13:55:42 -0800

    Change generic names to avoid confusion

--- aoc/Grid-Djikstra.swift
+++ aoc/Grid-Djikstra.swift
@@ -21,18 +21,18 @@ extension ScoreType {
 
 extension Int: ScoreType {}
 
-struct PossibleMove<Position: Equatable, Score: ScoreType>: Comparable {
-    let move: Position
+struct PossibleMove<State: Equatable, Score: ScoreType>: Comparable {
+    let move: State
     let score: Score
     static func < (lhs: PossibleMove, rhs: PossibleMove) -> Bool { lhs.score < rhs.score }
 }
 
 extension Grid {
-    func bestMoves<Position: Hashable, Score: Comparable>(from start: PossibleMove<Position, Score>, to end: (Position) -> Bool, generator: (Grid, PossibleMove<Position, Score>) -> [PossibleMove<Position, Score>]) -> [Position : Score] {
+    func bestMoves<State: Hashable, Score: Comparable>(from start: PossibleMove<State, Score>, to end: (State) -> Bool, generator: (Grid, PossibleMove<State, Score>) -> [PossibleMove<State, Score>]) -> [State : Score] {
 
-        var best: [Position : Score] = [start.move : start.score]
+        var best: [State : Score] = [start.move : start.score]
         var bestRoute: Score? = nil
-        var examine = Heap<PossibleMove<Position, Score>>()
+        var examine = Heap<PossibleMove<State, Score>>()
         examine.insert(start)
 
         while let possibility = examine.popMin() {
@@ -58,11 +58,11 @@ extension Grid {
         return best
     }
 
-    func bestMoves<Position: Hashable>(from start: Position, to end: (Position) -> Bool, generator: (Grid, PossibleMove<Position, Int>) -> [PossibleMove<Position, Int>]) -> [Position : Int] {
+    func bestMoves<State: Hashable>(from start: State, to end: (State) -> Bool, generator: (Grid, PossibleMove<State, Int>) -> [PossibleMove<State, Int>]) -> [State : Int] {
         return bestMoves(from: PossibleMove(move: start, score: 0), to: end, generator: generator)
     }
 
-    func moveDistance<Position: Hashable>(from start: Position, to end: Position, generator: (Grid, PossibleMove<Position, Int>) -> [PossibleMove<Position, Int>]) -> Int? {
+    func moveDistance<State: Hashable>(from start: State, to end: State, generator: (Grid, PossibleMove<State, Int>) -> [PossibleMove<State, Int>]) -> Int? {
         let best = bestMoves(from: PossibleMove(move: start, score: 0), to: { $0 == end }, generator: generator)
         return best[end]
     }
